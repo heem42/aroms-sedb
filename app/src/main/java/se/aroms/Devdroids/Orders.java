51267e04268641a0516e1d83015c5e682d92242b
Nabeel-FastNU <nabeelkhalid4078@gmail.com>
2019-11-30 15:11:57 +0500

    nistr prototype 3 completed

---
+++ app/src/main/java/se/aroms/Devdroids/Orders.java
@@ -0,0 +1,62 @@
+package se.aroms.Devdroids;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class Orders implements Serializable {
+    private Date orderTIme;
+    private List<orders_items> orderItems;
+    private String orderId;//unique order if
+    private String uid;//table no
+    private int status; //paid or not paid
+    public Orders(Date orderTIme, List<orders_items> orderItems, String orderId, String uid, int status) {
+        this.orderTIme = orderTIme;
+        this.orderItems = orderItems;
+        this.orderId = orderId;
+        this.uid = uid;
+        this.status=status;
+    }
+
+    public Orders(){}
+
+    public Date getOrderTIme() {
+        return orderTIme;
+    }
+
+    public void setOrderTIme(Date orderTIme) {
+        this.orderTIme = orderTIme;
+    }
+
+    public List<orders_items> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<orders_items> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
---
+++ app/src/main/java/se/aroms/Devdroids/orders_items.java
@@ -0,0 +1,51 @@
+package se.aroms.Devdroids;
+
+import java.io.Serializable;
+
+public class orders_items implements Serializable {
+    private String ItemID;
+    private String size;
+    private Double price;
+    private Double incured_price;
+
+    public orders_items(String itemID, String size, Double price, Double incured_price) {
+        ItemID = itemID;
+        this.size = size;
+        this.price = price;
+        this.incured_price = incured_price;
+    }
+
+    public orders_items(){}
+
+    public String getItemID() {
+        return ItemID;
+    }
+
+    public void setItemID(String itemID) {
+        ItemID = itemID;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getIncured_price() {
+        return incured_price;
+    }
+
+    public void setIncured_price(Double incured_price) {
+        this.incured_price = incured_price;
+    }
+}
